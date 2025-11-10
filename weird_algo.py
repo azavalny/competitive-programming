3baf6d03fcac7cc4e3c30bf3250ec4391fc5ca26
Alex Zavalny <57161600+azavalny@users.noreply.github.com>
2025-11-09 22:48:52 -0500

    Add files via upload

---
+++ weird_algo.py
@@ -0,0 +1,11 @@
+# weird algorithm
+
+num = int(input())
+
+while num > 1:
+    print(int(num), end=" ")
+    if num%2 == 0:
+        num = num/2
+    else:
+        num = 3*num + 1
+print(int(num), end=" ")
