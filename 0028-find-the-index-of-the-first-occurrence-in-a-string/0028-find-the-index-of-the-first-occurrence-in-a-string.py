2bda5fc024ce422e139463c50b62eb34c76558cb
Alex Zavalny <57161600+azavalny@users.noreply.github.com>
2025-12-10 22:24:00 -0500

    Time: 4 ms (3.62%), Space: 18.1 MB (11.56%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,12 +1,90 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        
-        for i in range(len(haystack)):
-            if haystack[i] == needle[0]:
-                window = 0
-                for j in range(len(needle)):
-                    if i + j < len(haystack) and haystack[i + j] == needle[j]:
-                        window +=1
-                if window == len(needle):
-                    return i
-        return -1
+
+        def constructLps(pat, lps):    
+            # len stores the length of longest prefix which 
+            # is also a suffix for the previous index
+            len_ = 0
+            m = len(pat)
+            
+            # lps[0] is always 0
+            lps[0] = 0
+
+            i = 1
+            while i < m:
+                
+                # If characters match, increment the size of lps
+                if pat[i] == pat[len_]:
+                    len_ += 1
+                    lps[i] = len_
+                    i += 1
+                
+                # If there is a mismatch
+                else:
+                    if len_ != 0:
+                        
+                        # Update len to the previous lps value 
+                        # to avoid redundant comparisons
+                        len_ = lps[len_ - 1]
+                    else:
+                        
+                        # If no matching prefix found, set lps[i] to 0
+                        lps[i] = 0
+                        i += 1
+
+        def search(pat, txt):
+            n = len(txt)
+            m = len(pat)
+
+            lps = [0] * m
+            res = []
+
+            constructLps(pat, lps)
+
+            # Pointers i and j, for traversing 
+            # the text and pattern
+            i = 0
+            j = 0
+
+            while i < n:
+                
+                # If characters match, move both pointers forward
+                if txt[i] == pat[j]:
+                    i += 1
+                    j += 1
+
+                    # If the entire pattern is matched 
+                    # store the start index in result
+                    if j == m:
+                        res.append(i - j)
+                        
+                        # Use LPS of previous index to 
+                        # skip unnecessary comparisons
+                        j = lps[j - 1]
+                
+                # If there is a mismatch
+                else:
+                    
+                    # Use lps value of previous index
+                    # to avoid redundant comparisons
+                    if j != 0:
+                        j = lps[j - 1]
+                    else:
+                        i += 1
+            return res
+
+        sol = search(needle, haystack)
+        if len(sol) == 0:
+            return -1
+        return sol[0]
+
+        # brute force O(n*m) ~= O(n^2)
+        # for i in range(len(haystack)):
+        #     if haystack[i] == needle[0]:
+        #         window = 0
+        #         for j in range(len(needle)):
+        #             if i + j < len(haystack) and haystack[i + j] == needle[j]:
+        #                 window +=1
+        #         if window == len(needle):
+        #             return i
+        # return -1
