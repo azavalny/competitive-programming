83479c88ab9daca71d7de0fcf1af2c1b5356445b
Alex Zavalny <57161600+azavalny@users.noreply.github.com>
2026-01-02 22:36:52 -0500

    Time: 11 ms (53.03%), Space: 19 MB (87.05%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,28 +1,28 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        neg = [n for n in nums if n*-1 > 0]
-        pos = [n for n in nums if n*1 > 0]
-        zeros = [n for n in nums if n == 0]
-        sol = zeros
+        n = len(nums)
+        res = [0] * n
+        l, r = 0, n - 1
+        write = n - 1
 
-        l = len(neg)-1
-        r = 0
-        while l >= 0 and r < len(pos):
-            if neg[l]**2 == pos[r]**2:
-                sol.append(neg[l]**2)
-                sol.append(pos[r]**2)
-                r +=1
-                l -= 1
-            elif neg[l]**2 > pos[r]**2:
-                sol.append(pos[r]**2)
-                r +=1
+        while l <= r:
+            left = nums[l]
+            right = nums[r]
+            if abs(left) > abs(right):
+                res[write] = left * left
+                l += 1
             else:
-                sol.append(neg[l]**2)
-                l -=1
-        while l >= 0:
-            sol.append(neg[l]**2)
-            l -=1
-        while r < len(pos):
-            sol.append(pos[r]**2)
-            r +=1
-        return sol
+                res[write] = right * right
+                r -= 1
+            write -= 1
+
+        return res
+
+        """
+nums = [-4, -1, 0, 3, 10]
+                l
+                r
+                w
+        [     1  9  16  100]
+
+        """ 
