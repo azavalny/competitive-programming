eb471105e154af7f2076e97432e3b5e0ed3101dc
Alex Zavalny <57161600+azavalny@users.noreply.github.com>
2026-02-01 13:11:48 -0500

    Time: 5 ms (57.64%), Space: 26.7 MB (21.18%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -12,13 +12,30 @@ class Solution:
                 shift every element to right O(n)
                 first element = original last element
 
-        sol 2:
+        for i in range(k): #O(nk) ~= O(n^2)
+            last = nums[-1]
+            nums.pop()
+            nums.insert(0, last) #O(n)
+
+        sol 2: O(n)
+            reverse nums
+            reverse first k elements
+            reverse the rest of the k elements
+
+            [7,6,5,4,3,2,1] k = 3
+            [5, 6, 7,4,3,2,1] reverse 1st k
+            [5, 6, 7, 1, 2, 3, 4] reverse nums[k:]
+
 
         sol 3:
             
         """
-
-        for i in range(k):
-            last = nums[-1]
-            nums.pop()
-            nums.insert(0, last)
+        k%=len(nums)
+        def rev(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+                r-=1
+        rev(0, len(nums)-1)
+        rev(0, k-1)
+        rev(k, len(nums)-1)
