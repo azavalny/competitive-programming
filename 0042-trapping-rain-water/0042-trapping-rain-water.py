51242c42335529c82d88c92bdc63f915075d0157
Alex Zavalny <57161600+azavalny@users.noreply.github.com>
2026-02-01 23:11:55 -0500

    Time: 9 ms (64.39%), Space: 21.1 MB (39.14%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,6 +1,10 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         """
+        [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+
+
+
         potential - height
         min(max_left[l], max_right[r]) - height
 
@@ -9,7 +13,6 @@ class Solution:
    R= [  3  3  3  3  3  3  3  2  2  2  1  0]
 
 S = 6
-        """
         l_wall = r_wall = 0
         max_left = [0]*len(height)
         max_right = [0]*len(height)
@@ -25,3 +28,20 @@ S = 6
             potential = min(max_left[i], max_right[i])
             sol += max(0, potential - height[i])
         return sol
+        """
+
+        l, r = 0, len(height)-1
+        lMax,rMax=height[l],height[r]
+        sol = 0
+
+        while l < r:
+            if lMax < rMax:
+                l+=1
+                lMax = max(lMax, height[l])
+                sol += lMax - height[l]
+            else:
+                r-=1
+                rMax = max(rMax, height[r])
+                sol += rMax - height[r]
+        return sol
+
