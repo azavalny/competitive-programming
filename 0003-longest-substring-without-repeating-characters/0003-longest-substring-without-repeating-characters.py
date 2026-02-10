fee5d7621e8ab8cd4e91a68586a0491a3016f616
Alex Zavalny <57161600+azavalny@users.noreply.github.com>
2026-02-09 22:56:17 -0500

    Time: 233 ms (8.06%), Space: 19.5 MB (5.07%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,30 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+
+        a b c a b c b b
+                      l
+                       r
+        increment r for every character
+        once duplicate is found:
+            move left pointer right until duplicate is gone
+
+        b b b b b
+              L
+                R 
+        p w w k e w
+              L
+                  R 
+
+        """
+        sol = 0
+        l = 0
+        curSet = set()
+        for r in range(len(s)):
+            print(s[l], s[r], curSet)
+            while s[r] in curSet:
+                curSet.remove(s[l])
+                l+=1
+            curSet.add(s[r])
+            sol = max(sol, r-l+1)
+        return sol
