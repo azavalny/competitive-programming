8c850377fe9bf56efd69d367f2b12647576b6f4e
Alex Zavalny <57161600+azavalny@users.noreply.github.com>
2026-03-22 13:09:29 -0400

    Time: 35 ms (55.24%), Space: 23.5 MB (8.49%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,13 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # [1, 1, 1, 1, 1, 1] k=3
-        # [1, 2, 3, 4, 5, 6]]
-        sol=0
+        """
+        prefix[j] - prefix[i] == k
+        prefix[j] -k = prefix[i] 
+
+        does the complement of prefix[j] -k exist?
+        """
+        table = defaultdict(int, {0: 1}) #{ curSum: counts}
+        sol = 0
         curSum = 0
-        prefixSums = {0:1 }
+
         for n in nums:
             curSum +=n
-            if curSum-k in prefixSums:
-                sol += prefixSums.get(curSum-k)
-            prefixSums[curSum] = 1 + prefixSums.get(curSum, 0)
+            sol += table[curSum - k] # increment solution w/ frequency if curSum-k exists else increment by 0 if not
+            table[curSum] +=1
         return sol
