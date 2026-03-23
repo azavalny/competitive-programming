3866353d038534412c3ad2e3932314241199991d
Alex Zavalny <57161600+azavalny@users.noreply.github.com>
2026-03-22 21:32:15 -0400

    Time: 75 ms (77.73%), Space: 25.4 MB (43.92%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,52 +1,30 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         """
-        max length of subarray with # 0's = # 1's
+        0's == 1's
+        0's - 1's == 0
 
-        [ 1 1 1 0 0]
-#1s: 3
-#0s: 2
-diff: index
-{
-    1:0
-    2: 1
-    3: 2
-}
-3-2=1 : 0 -4 = 3 solution
+        {diffs: first index}
 
-
-            [0, 1, 0]
-1s: 1
-0s: 2
-{
-    0:1
-    1: 0
-}
-
-2-1 = 1 : 0 - 2 = 2
-
-            [0, 1, 1, 1, 1, 1, 0, 0, 0]
-                                     i
-numZeros    1   1
-
+num:     0 1  1  1  1  1  0  0  0
+index:   0 1  2  3  4  5  6  7  8
+diff:    1 0 -1 -2 -3 -4 -3 -2 -1
+{1: 0
+0: 1
+-1: 2
+-2: 3
+-3: 4}
+        if diffs match compare curIndex - first index
         """
-
-        zeros=0
-        ones=0
-        diffs={} # count[1] - count[0]: ending index
-        sol=0
-
-        for i, n in enumerate(nums):
-            if n == 0:
-                zeros+=1
-            if n == 1:
-                ones+=1
-                
-            if ones - zeros not in diffs:
-                diffs[ones - zeros] = i
-            # best case if end
-            if ones == zeros:
-                sol = ones + zeros
+        table = defaultdict(int, {0: -1})
+        diff = 0
+        sol = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                diff-=1
             else:
-                sol = max(sol, i - diffs[ones - zeros])
+                diff+=1
+            if diff not in table:
+                table[diff] = i
+            sol = max(sol, i-table[diff])
         return sol
