4ce662c8ed993f1d842889abdcc62aebc58df67d
Alex Zavalny <57161600+azavalny@users.noreply.github.com>
2026-03-31 21:56:04 -0400

    Time: 43 ms (98.33%), Space: 32.9 MB (62.34%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
@@ -4,8 +4,6 @@ class Solution:
         valid split  = sum first i+1 elements >= sum last n-i-1 elements 
             at least 1 element right of i <-- we have to stop at len(nums)-2
 
-        # valid splits = { prevSum : counts}
-
         prevSum >= curSum
 
         [10, 4, -8, 7]
@@ -15,19 +13,13 @@ endSum   13   3  -1  7
 curSum   2  5  6  6
 endSum   6  4  1  0
         """
-        from collections import deque
-        prefixSums = deque()
+        total = sum(nums)
         curSum = 0
-        for n in nums:
-            curSum +=n
-            prefixSums.append(curSum)
-        suffixSums = deque()
-        curSum = 0
-        for n in reversed(nums):
-            curSum += n
-            suffixSums.appendleft(curSum)
         sol = 0
-        for i in range(len(nums)-2, -1, -1):
-            if prefixSums[i] >= suffixSums[i+1]:
+
+        for i in range(len(nums)-1):
+            curSum += nums[i]
+            prevSum = total - curSum
+            if curSum >= prevSum:
                 sol +=1
         return sol
