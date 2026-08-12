08899c4016e43e6e4a15d8a5b4a8f12d7ecba441
Alex Zavalny <57161600+azavalny@users.noreply.github.com>
2026-08-11 22:40:05 -0400

    Time: 62 ms (5.47%), Space: 31.2 MB (94.24%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        """
+
+        [-4, 3, -2, 5, -8, 6, 2, -1, 4]
+                           l
+                                      r
+curSum: -4   3   1  6  -2  6  8   7  11
+maxSum: -4   3   3  6   6  6  8   8  11
+
+    while r < len(nums):
+        while curSum negative and left < right:
+            shrink window and update current sum
+        update current sum and increment right
+        """
+        curSum, maxSum, l, r = 0, float("-inf"), 0, 0
+        while r < len(nums):
+            while curSum < 0 and l < r:
+                curSum -= nums[l]
+                l+=1
+            curSum += nums[r]
+            maxSum = max(maxSum, curSum)
+            r+=1
+        return maxSum
