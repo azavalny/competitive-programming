b247b319ce5c62bc1fc771e5d5a72c332b3c4f5e
Alex Zavalny <57161600+azavalny@users.noreply.github.com>
2026-09-07 22:47:17 -0400

    Time: 75 ms (29.13%), Space: 29.1 MB (24.8%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
@@ -10,18 +10,31 @@ class Solution:
         find # of negative values
         if odd, assign least absolute value negative
         flip everything else positive
+
+        abs_sum = 0
+        neg_count = 0
+        min_abs_value = float('inf')
+        ROWS, COLS = len(matrix), len(matrix[0])
+        for row in range(ROWS):
+            for col in range(COLS):
+                abs_sum += abs(matrix[row][col])
+                if matrix[row][col] < 0:
+                    neg_count += 1
+                min_abs_value = min(min_abs_value, abs(matrix[row][col]))
+        if neg_count % 2 != 0: #odd
+            abs_sum -= 2*min_abs_value
+        return abs_sum
         """
-        absMatrix = [[abs(c) for c in r] for r in matrix]
-        absSum = sum([sum(row) for row in absMatrix])
         numNegatives = 0
-        for r in matrix:
-            for c in r:
-                if c < 0:
-                    numNegatives+=1
         leastAbsVal = float("inf")
-        for r in matrix:
-            for c in r:
-                leastAbsVal = min(leastAbsVal, abs(c))
+        ROWS, COLS = len(matrix), len(matrix[0])
+        absSum=0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] < 0:
+                    numNegatives+=1
+                leastAbsVal = min(leastAbsVal, abs(matrix[r][c]))
+                absSum += abs(matrix[r][c])
         if numNegatives%2 != 0:
             absSum -= 2*leastAbsVal
         return absSum
